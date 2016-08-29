eceeb85f04dd92c82710c9a33157c8ae653e268d
Kevin <tig3r86@gmail.com>
2016-08-29 17:59:08 +0930

    add params for google oauth

--- src/globalVar.js
+++ src/globalVar.js
@@ -3,8 +3,8 @@ export const googleOauthParams = {
   url: `${apiUrl}/oauth/google`,
   authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
   params: {
-    client_id: process.env.GOOGLE_ID,
-    redirect_uri: process.env.GOOGLE_REDIRECT_URL,
+    client_id: '781005171750-aulvo8dfukpk701v9je8h12rohj5gqg7.apps.googleusercontent.com',
+    redirect_uri: 'https://pollwise.herokuapp.com/oauth/google/callback',
     scope: 'openid profile email',
     response_type: 'code'
   }
